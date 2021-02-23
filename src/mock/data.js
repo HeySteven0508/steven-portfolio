0ea5082eec28559014ec59b13984a297d3ed54eb
HeySteven0508 <concentratify@gmail.com>
2021-02-23 17:15:00 +0800

    Update head Data

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,9 +2,9 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Steven Tan | Java Developer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'My online Portfolio', // e.g: Welcome to my website
 };
 
 // HERO DATA
